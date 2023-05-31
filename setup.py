ba30e47a310ad0ea3438b76ad40532a34e8f7cb2
Deepak Porwal <36986305+Deepak-porwal04@users.noreply.github.com>
2023-05-31 11:50:14 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='wordlist',
+    version='0.1',
+    author='Deepak Porwal',
+    author_email='dporwal04@gmail.com',
+    description='A list of words',
+    packages=['wordlist'],
+    url='https://github.com/deepak-porwal04/wordlist',
+    )
